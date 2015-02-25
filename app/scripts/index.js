73890d8770387740f79dd778eacf8c4ca7d222b5
Luke Lancaster <luketlancaster@gmail.com>
2015-02-24 20:37:29 -0600

    only display 20 messages. Also display time

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -7,9 +7,10 @@ var fb = new Firebase('https://chatappy.firebaseio.com/');
 $('#chatForm').on('click', '#chatButton', function(evt) {
 	evt.preventDefault();
 	var   chatWords = $('#chatInput').val(),
-	    chatterName = $('#chatterName').val();
+	    chatterName = $('#chatterName').val(),
+	            now = moment().format('MMM Do YYYY, h:mm a');
 
-	fb.push({name: chatterName, message: chatWords});
+	fb.push({name: chatterName, message: chatWords, time: now});
 
 	$('input').val('');
 });
@@ -17,19 +18,25 @@ $('#chatForm').on('click', '#chatButton', function(evt) {
 fb.on('child_added', function(snap){
 	var content = snap.val();
 
-	addMessages(content.name, content.message);
+	addMessages(content.name, content.message, content.time);
 });
 
-function addMessages(name, message) {
+function addMessages(name, message, now) {
 	$('<div class="nameAndMessage"></div>')
 		.text(message)
 		.prepend(
-			$('<strong></strong>').text(name + ': ')
+			$('<strong></strong>').text(name + ' said: ')
 			)
+		.prepend('On ' + now + ' ')
 		.appendTo($('.messagesBox'));
+		removeTopMessage();
 }
 
-function countLength() {
-	//count length of .nameAndMessage divs
+function removeTopMessage() {
+	//count length of .nameAndMessage divs √
+	var divsLength = $('.nameAndMessage').length;
 	//remove the first .nameAndMessage div
+	if(divsLength >= 20) {
+		$('.nameAndMessage:first').remove();
+	}
 }
